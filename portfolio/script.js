98380b29443e50b3a0f47218d4f259cf62cbda0d
GOLU KUMAR <prajaptigolu07@gmail.com>
2024-11-26 10:22:18 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Developer", "UI/UX Designer","web designer", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
